0fdbb3397c59946cad72b46195c3c687b01db026
Hamed Zare <56786033+Hamed1zare@users.noreply.github.com>
2022-04-13 14:16:28 +0430

    Add files via upload

---
+++ Calculator/calculator/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        double r1, r2, sum, min, mul, div;
+        string op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                r1 = double.Parse(textBox1.Text);
+                textBox1.Text = "";
+                op = "-";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                r1 = double.Parse(textBox1.Text);
+                textBox1.Text = "";
+                op = "*";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                r1 = double.Parse(textBox1.Text);
+                textBox1.Text = "";
+                op = "/";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                r2 = double.Parse(textBox1.Text);
+                switch(op)
+                {
+                    case "+":
+                        textBox1.Text = (r1 + r2).ToString();
+                        break;
+
+                    case "-":
+                        textBox1.Text = (r1 - r2).ToString();
+                        break;
+
+                    case "*":
+                        textBox1.Text = (r1 * r2).ToString();
+                        break;
+
+                    case "/":
+                        textBox1.Text = (r1 / r2).ToString();
+                        break;
+                        return;
+
+
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+       
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text !="")
+            {
+                r1 = double.Parse(textBox1.Text);
+                textBox1.Text = "";
+                op = "+";
+            }
+        }
+    }
+}
---
+++ Guess-Number/Guess-Number/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_Number
+{
+    public partial class Form1 : Form
+    {
+        Random r;
+        int computer_number;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+            computer_number = r.Next(0, 100);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int user_number = Convert.ToInt32(textBox1.Text);
+            if(user_number==computer_number)
+            {
+                label1.Text = "you win ";
+                label2.Text = Convert.ToString(count);
+            }
+                else if (user_number>computer_number)
+                {
+                    label1.Text = "go down";
+                count++;
+                }
+            else if (user_number< computer_number)
+            {
+                label1.Text = "go up";
+                count++;
+            }
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
